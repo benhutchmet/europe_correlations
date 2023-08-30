dd63b32ca40e4f47eb0f7a61833e7cd41f9825a5
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2023-08-30 09:54:00 +0100

    first commit of set up files

---
+++ dictionaries.py
@@ -0,0 +1,15 @@
+# Define the dimensions for the gridbox for the North Sea Region
+north_sea_grid = {
+    'lon1': -2,
+    'lon2': 8,
+    'lat1': 50,
+    'lat2': 65
+}
+
+# Define the dimensions for the gridbox for Central Europe
+central_europe_grid = {
+    'lon1': 5,
+    'lon2': 35,
+    'lat1': 44,
+    'lat2': 57
+}
